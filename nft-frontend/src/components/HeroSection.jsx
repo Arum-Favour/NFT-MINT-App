aec4a2e4dfbd47f7ebe45b28a4ea79866598456c
Favour.sol <132628823+Arum-Favour@users.noreply.github.com>
2025-02-15 15:06:06 +0100

    Update HeroSection.jsx

--- nft-frontend/src/components/HeroSection.jsx
+++ nft-frontend/src/components/HeroSection.jsx
@@ -1,24 +1,24 @@
-import React from "react";
 import rocketIcon from "../assets/rocketIcon.png";
 import playIcon from "../assets/playIcon.png";
 
 const HeroSection = () => {
   return (
-    <div className="flex justify-center items-center w-4/5 mx-auto mt-20">
-      <div className="flex flex-col justify-center items-center w-2/5 mx-auto gap-5">
-        <h1 className="animate-pulse text-5xl font-bold text-white text-center">
+    <div className="flex justify-center items-center w-full sm:w-4/5 mx-auto mt-10 sm:mt-20 px-4 max-w-screen-lg">
+      <div className="flex flex-col justify-center items-center w-full sm:w-2/5 mx-auto gap-5 text-center">
+        <h1 className="animate-pulse text-3xl sm:text-5xl font-bold text-white">
           Discover & Collect Extraordinary NFTs
         </h1>
-        <p className="text-[#D1D5DB] text-center mt-4">
-          Explore unique NFTs created by artists worldwide.Enter the world of
+        <p className="text-[#D1D5DB] mt-2 sm:mt-4">
+          Explore unique NFTs created by artists worldwide. Enter the world of
           digital art and collectibles.
         </p>
-        <div className="flex justify-evenly items-center w-full mt-1">
-          <button className="flex items-center cursor-pointer text-white text-sm font-bold bg-gradient-to-r from-[#EC4899] to-[#8B5CF6] justify-center py-1 px-0.5 h-[58px] gap-1 rounded-[12px] w-[194px]">
-            <img src={rocketIcon} alt="rocket Icon" /> Start Creating
+        <div className="flex flex-col sm:flex-row justify-center items-center w-full gap-3 sm:gap-5 mt-4">
+          <button className="flex items-center cursor-pointer text-white text-sm font-bold bg-gradient-to-r from-[#EC4899] to-[#8B5CF6] justify-center py-3 px-2 gap-2 rounded-[12px] w-full sm:w-[194px] hover:animate-bounce">
+            <img src={rocketIcon} alt="rocket Icon" className="w-5 h-5" />
+            Start Creating
           </button>
-          <button className="flex items-center cursor-pointer text-white text-sm font-bold bg-[#1F293780] justify-center py-1 px-0.5 h-[58px] gap-1 rounded-[12px] w-[194px] border-[#1F293780]">
-            <img src={playIcon} alt="Play button Icon" />
+          <button className="flex items-center cursor-pointer text-white text-sm font-bold bg-[#1F293780] justify-center py-3 px-2 gap-2 rounded-[12px] w-full sm:w-[194px] border border-[#1F293780]">
+            <img src={playIcon} alt="Play button Icon" className="w-5 h-5" />
             Watch Demo
           </button>
         </div>
