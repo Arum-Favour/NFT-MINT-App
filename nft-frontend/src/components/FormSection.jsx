9e950ad2620dc69e4237c340391447b5e84f693d
Favour.sol <132628823+Arum-Favour@users.noreply.github.com>
2025-02-18 00:49:27 +0100

    Update FormSection.jsx

--- nft-frontend/src/components/FormSection.jsx
+++ nft-frontend/src/components/FormSection.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import MintIcon from "../assets/MintIcon.png";
 import CheckIcon from "../assets/CheckIcon.png";
 import ShareIcon from "../assets/ShareIcon.png";
